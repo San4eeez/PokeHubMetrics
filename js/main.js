846f95294fa987640d5df1573dfd9f4564a25a2c
San4eeez <AlexandroHell@yandex.ru>
2024-01-15 00:19:17 +0300

    Для работы с апи на сайте

---
+++ js/main.js
@@ -0,0 +1,29 @@
+async function getResponse(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/photos')
+    let content = await response.json()
+    content = content.splice(0,10)
+    let list = document.querySelector('.posts')
+
+
+
+
+
+
+    let key;
+
+    for (key in content){
+
+        list.innerHTML += `
+        <li class="post">
+            <h4>${content[key].title}</h4>
+            <img src="${content[key].url}">
+        </li>
+        `
+
+       content[key]
+    }
+
+}
+
+getResponse()
+
